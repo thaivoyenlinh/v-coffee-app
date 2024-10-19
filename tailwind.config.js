4df64c5e4b645b6bb75fc29228bb970691234285
TVYLi <thaivoyenlinh007@gmail.com>
2024-10-20 04:31:31 +0700

    feat(core): setup tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./src/**/*.{html,js,ts,jsx,tsx}"],
+  theme: {
+    extend: {
+      colors: {
+        primary: "#875435",
+      },
+    },
+  },
+  plugins: [],
+};
